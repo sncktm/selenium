8caed13670f9587dee94f87d31fa1b6abecf2ac9
KCSNET\st20224116 <sncktm01@gmail.com>
2025-06-27 10:29:26 +0900

    タイムセール一覧、詳細の追加

---
+++ tests/time_sale_list.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+
+def run_test(driver):
+   
+    time.sleep(3)
+    
+    driver.find_element(By.LINK_TEXT, "タイムセール管理").click()
+
+    time.sleep(3)
+
+    # タイムセール一覧をクリック
+    driver.find_element(By.LINK_TEXT, "タイムセール一覧").click()
+    
+    time.sleep(3)
+
+    # タイムセール詳細をクリック
+    driver.find_element(By.CLASS_NAME, "detail-button").click()
+
+    # 指定した秒数だけ待つ
+    time.sleep(5)
